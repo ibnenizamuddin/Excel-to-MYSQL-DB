63503be801d43c8150638907b8230cbe8c30fc4b
Ehsanullah Shaikh <ibnenizamuddin@gmail.com>
2021-03-13 05:24:25 -0500

    script to load file to DB

---
+++ migration.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import mysql.connector
+from sqlalchemy import create_engine
+from os.path import join, dirname, realpath
+import os
+# Solution:
+path = "D:/ecw_install/EXCEL Migration To DB/migration/data/"
+listoffile = os.listdir(path)
+
+for file in listoffile:
+    print(file)
+    filename = path + file
+    x = file.split(".")
+    tablename = x[0]
+    engine = create_engine(
+        'mysql+mysqlconnector://ecwDbAdmin:chXw$PBu5jtm@10.211.21.152:4950/dcmove', pool_recycle=3600, pool_size=5, echo=False)
+    df = pd.read_excel(filename, dtype=str, keep_default_na=False)
+    df.to_sql(tablename, con=engine, if_exists='append',chunksize=100,index=False)
